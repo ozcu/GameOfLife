7a92e1c6750aae11e7fece8a20769c5b9a4cbc7f
ugurhan <ugurhan.buyukozcu@gmail.com>
2022-02-11 21:44:25 +0300

    Fixed rules

--- src/script.js
+++ src/script.js
@@ -1,29 +1,23 @@
 import './style.css'
 import p5 from "p5"
 
-
-
 const sketch = p5 => {
-
-  //Time
-  const numFrames = 100
-  let sum= 0
 
   // Variables scoped within p5
   const canvasWidth = p5.windowWidth
   const canvasHeight = p5.windowHeight
 
+  console.log(canvasWidth,canvasHeight)
 
   // make library globally available
   window.p5 = p5
 
   //Grid
   let grid,cols,rows
-  let resolution = 50
+  let resolution = 20
 
-  cols = 500/resolution
-  rows = 500/resolution
-  
+  cols = 600/resolution
+  rows = 600/resolution
 
   //Array
   function make2DArray(cols,rows){
@@ -40,11 +34,10 @@ const sketch = p5 => {
     for(let i =-1 ; i<2 ;i++){
       for(let j =-1; j<2 ;j++){
         sum += grid[x+i][y+j]
-        
       }
     }
     
-    sum -= grid[x][y]      
+    sum -= grid[x][y]     
     return sum
   }
 
@@ -53,7 +46,7 @@ const sketch = p5 => {
 
   p5.setup = () => {
 
-    p5.createCanvas(500, 500)
+    p5.createCanvas(canvasWidth,canvasHeight)
 
     //Initialization
     grid  = make2DArray(cols,rows)
@@ -62,7 +55,6 @@ const sketch = p5 => {
         grid[i][j] = p5.floor(p5.random(2))
       }
     }
-
   }
 
 
@@ -71,7 +63,6 @@ const sketch = p5 => {
     p5.background(0)
     
     //Draw first Gen
-    
     for(let i = 0 ; i<cols; i++){
       for(let j = 0; j<rows; j++){
         let x = i * resolution
@@ -81,54 +72,45 @@ const sketch = p5 => {
           p5.fill(255)
           p5.stroke(0)
           p5.rect(x,y,resolution-1,resolution-1)
-          
         }
       }
     }
 
-    if(p5.frameCount%10 == 0){
+
 
     //Compute next based on grid
     
     let next = make2DArray(cols,rows)
-  
 
     for(let i = 1 ; i<cols ;i++){
-      for(let j = 1; j <rows ;j++){
+      for(let j = 1; j<rows ;j++){
 
         let state = grid[i][j]
 
         //Edges
         if(i == 0 || i == cols-1 || j == 0 || j == rows-1 ){
           next[i][j] = state
-          
+     
         }else{
 
-        //Count neighbors
-        let neighbors =  countNeighbors(grid,i,j)
-        
-        //Rules
-        if( state == 0 && neighbors == 3 ){
-          next[i][j] == 1
-        }else if( state == 1 && (neighbors < 2 || neighbors > 3) ){
-          next[i][j] = 0
-        }else {
-          next[i][j] = state
-        }
-      }
-      grid[i][j]=next[i][j]
-      }
-      
-    }
-    
-    // nexGen to become prevGen
-    
-  }
-
+            //Count neighbors
+            let neighbors =  countNeighbors(grid,i,j)
+            
+            //Rules
+            if( state == 0 && neighbors == 3 ){
+              next[i][j] = 1
+            }else if( state == 1 && (neighbors < 2 || neighbors > 3) ){
+              next[i][j] = 0
+            }else {
+              next[i][j] = state
+            }
+          }
+        } 
+     }
+      // nexGen to become prevGen
+      grid=next
+   }
 }
-   
-}
-
 
 
 new p5(sketch)
@@ -136,6 +118,6 @@ new p5(sketch)
 export default sketch
 
 p5.windowResized = () => {
-    resizeCanvas(windowWidth, windowHeight)
+    resizeCanvas(canvasWidth, canvasHeight)
     
 }
